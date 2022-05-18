b72b9ea96ff9be8020a1a27d9319ce9c0d60d50b
astuka <jrobins855@gmail.com>
2022-05-17 21:47:56 -0700

    Added rudimentary level up script

--- game.js
+++ game.js
@@ -27,9 +27,16 @@ const bread = new Item("bread",10,0,0.5);
 const knife = new Item("knife",0,10,0.5);
 items = [bread, knife]
 
+const player = new Character(100,100,0,100,1,[]);
 
 function game(){
-    const player = new Character(100,100,0,100,1,[]);
+    if (player.exp >= player.max_exp){
+        player.level = player.level + 1;
+        player.exp = player.exp - player.max_exp;
+        player.max_exp = player.max_exp + 50;
+        player.max_health = player.max_health + 50;
+        player.health = player.max_health;
+    }
     let stats = "You are a level "+player.level+" survivor. You have "+player.health+"/"+player.max_health+" health and "+player.exp+"/"+player.max_exp+" XP.";
     document.getElementById("character").innerHTML=stats;
 }
@@ -39,7 +46,9 @@ document.getElementById("search").onclick = function() {search()};
 function search(){
     let found_item = Math.floor(Math.random() * items.length);
     let result = "You found a "+items[found_item].item_name;
+    player.exp = player.exp + 10;
     document.getElementById("search_results").innerHTML = result;
+    game();
 }
 
 
