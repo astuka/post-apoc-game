d3365fb43aae1caaca9b91a4835ed68ab2ce89f2
astuka <jrobins855@gmail.com>
2022-05-17 21:39:58 -0700

    Initial commit

---
+++ game.js
@@ -0,0 +1,46 @@
+
+
+//character stats
+class Character{
+    constructor(health,max_health,exp,max_exp,level,inventory){
+        this.health = health;
+        this.max_health = max_health;
+        this.exp = exp;
+        this.max_exp = max_exp;
+        this.level = level;
+        this.inventory = inventory;
+    }
+}
+
+//in-game item
+class Item{
+    constructor(item_name, health_add,health_subtract, probability){
+        this.item_name = item_name;
+        this.health_add = health_add;
+        this.health_subtract = health_subtract;
+        this.probability = probability;
+    }
+}
+
+//item list
+const bread = new Item("bread",10,0,0.5);
+const knife = new Item("knife",0,10,0.5);
+items = [bread, knife]
+
+
+function game(){
+    const player = new Character(100,100,0,100,1,[]);
+    let stats = "You are a level "+player.level+" survivor. You have "+player.health+"/"+player.max_health+" health and "+player.exp+"/"+player.max_exp+" XP.";
+    document.getElementById("character").innerHTML=stats;
+}
+
+document.getElementById("search").onclick = function() {search()};
+
+function search(){
+    let found_item = Math.floor(Math.random() * items.length);
+    let result = "You found a "+items[found_item].item_name;
+    document.getElementById("search_results").innerHTML = result;
+}
+
+
+window.onload = game();
